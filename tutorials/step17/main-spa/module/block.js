1585401c0781033ceedec9196f8e84cdd188bfdb
mogera551 <mogera551@gmail.com>
2022-04-29 08:41:24 +0900

    [add]tutorial

---
+++ tutorials/step17/main-spa/module/block.js
@@ -0,0 +1,28 @@
+
+const fruits = [
+  "apple",
+  "banana",
+  "orange",
+  "strawberry",
+];
+
+const context = {};
+class ViewModelClass {
+  "@fruits" = fruits;
+  "@fruits.*";
+  "@@newFruits" = "";
+  get "isEmpty"() {
+    return !this["newFruits"];
+  }
+  onClickAdd(event) {
+    this["fruits"].push(this["newFruits"]);
+    this["newFruits"] = "";
+    context.notify("fruits");
+  }
+}
+
+const dependencyRules = [
+  ["isEmpty", ["newFruits"]],
+];
+
+export default { ViewModelClass, dependencyRules, context }
---
+++ tutorials/step18/main-spa/module/block.js
@@ -0,0 +1,27 @@
+
+const fruits = [
+  "apple",
+  "banana",
+  "orange",
+  "strawberry",
+];
+
+class ViewModelClass {
+  "@fruits" = fruits;
+  "@fruits.*";
+  "@@newFruits" = "";
+  get "isEmpty"() {
+    return !this["newFruits"];
+  }
+  set eventClickAdd(event) {
+    this["fruits"].push(this["newFruits"]);
+    this["newFruits"] = "";
+  }
+}
+
+const dependencyRules = [
+  ["isEmpty", ["newFruits"]],
+  ["fruits", ["eventClickAdd"]],
+];
+
+export default { ViewModelClass, dependencyRules }
