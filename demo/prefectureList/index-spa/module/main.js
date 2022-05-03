43447e05c9a337a3369fbc5836bdee40460743f8
mogera551 <mogera551@gmail.com>
2022-05-03 21:57:04 +0900

    [update]@property#init

--- demo/prefectureList/index-spa/module/main.js
+++ demo/prefectureList/index-spa/module/main.js
@@ -1,6 +1,6 @@
 const context = {};
 class ViewModelClass {
-  "@@prefectures#init" = async data => await fetch(data.apiGetList).then(response => response.json());
+  "@prefectures#init" = async data => await fetch(data.apiGetList).then(response => response.json());
   "@prefectures.*.no#get" = () => Number(context.$1) + 1;
   "@prefectures.*.name";
   "@prefectures.*.capital";
--- src/ViewModel/Initializer.js
+++ src/ViewModel/Initializer.js
@@ -4,7 +4,9 @@ export default class Initializer {
   static async init(context, data, viewModel = context.viewModel, properties = context.properties) {
     const promises = [];
     for(const property of properties.values) {
-      property?.init && promises.push(property.init(data).then(value => viewModel[property.name] = value));
+      property?.init && promises.push(property.init(data).then(value => {
+        return property.name.includes(".") ? (viewModel[property.name] = value) : (viewModel[`$$${property.name}`] = value);
+      }));
     }
     await Promise.all(promises);
     return eventHandler.exec(viewModel, "init", data);
