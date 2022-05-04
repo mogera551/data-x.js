371b9d8ef817831b223c8accc05eb7d66420715e
mogera551 <mogera551@gmail.com>
2022-05-05 08:20:15 +0900

    [update]demo/prefectureList

--- demo/prefectureList/index-spa/module/main.js
+++ demo/prefectureList/index-spa/module/main.js
@@ -1,12 +1,28 @@
 const context = {};
 class ViewModelClass {
   "@prefectures#init" = async data => await fetch(data.apiGetList).then(response => response.json());
-  "@prefectures.*.no#get" = () => Number(context.$1) + 1;
-  "@prefectures.*.name";
-  "@prefectures.*.capital";
-  "@prefectures.*.population";
-  "@prefectures.*.sharePopulation#get" = () => this["prefectures.*.population"] / this["sumPopulation"] * 100;
-  "@sumPopulation#get" = () => this["prefectures"].reduce((sum, pref, i) => sum + this[`prefectures.${i}.population`], 0);
+  "@prefectures.*.region";
+
+  "@regions#get" = () => Array.from(new Set(this["prefectures"].map((p, i) => this[`prefectures.${i}.region`])));
+  "@regions.*";
+  
+  "@@chosenRegion" = "";
+  
+  "@filteredPrefectures#get" = () => this["prefectures"].filter((p, i) => this["chosenRegion"] ? this["chosenRegion"] === this[`prefectures.${i}.region`] : true);
+  "@filteredPrefectures.*.no#get" = () => Number(context.$1) + 1;
+  "@filteredPrefectures.*.name";
+  "@filteredPrefectures.*.capital";
+  "@filteredPrefectures.*.population";
+  "@filteredPrefectures.*.sharePopulation#get" = () => this["filteredPrefectures.*.population"] / this["sumPopulation"] * 100;
+  
+  "@sumPopulation#get" = () => this["filteredPrefectures"].reduce((sum, p, i) => sum + this[`filteredPrefectures.${i}.population`], 0);
+  "@sumTitle#get" = () => this["chosenRegion"] ? `${this["chosenRegion"]}合計`: "全国合計";
 }
 
-export default { ViewModelClass, context }
+const dependencyRules = [
+  [ "filteredPrefectures", [ "chosenRegion" ] ],
+  [ "sumPopulation", [ "chosenRegion" ] ],
+  [ "sumTitle", [ "chosenRegion" ] ],
+];
+
+export default { ViewModelClass, dependencyRules, context }
