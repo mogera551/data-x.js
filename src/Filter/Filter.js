c3ec0a1f9a066cb1c25480af37623c52775b8e8a
mogera551 <mogera551@gmail.com>
2022-05-04 11:35:42 +0900

    [fix]local filter, call regist.js

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,4 +1,14 @@
 import Filters from "./Filters.js";
 import "./Builtin.js";
+import App from "../App.js"
+
+async function registLocalFilter() {
+  if (App.options.localFilter) {
+    const index = document.baseURI.lastIndexOf("/");
+    const base = (index >= 0) ? document.baseURI.slice(0, index + 1) : "";
+    await import(`${base}${App.options.filterPath}/regist.js`);
+  }
+}
+await registLocalFilter();
 
 export default Filters;
