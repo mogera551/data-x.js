efceaaf1c709a1b38934d7f2884364a1309738c3
mogera551 <mogera551@gmail.com>
2022-05-08 07:41:19 +0900

    [update]demo refactoring

--- demo/Users/index-spa/module/users.js
+++ demo/Users/index-spa/module/users.js
@@ -1,12 +1,12 @@
 const context = {};
 class AppViewModel {
   "@show#get" = () => this.$content === "users";
-  "@@users#get" = () => this.$userList.getUsers();
+  "@users#get" = () => this.$userList.getUsers();
   "@users.*.id";
   "@users.*.name";
   "@users.*.email";
 
-  "#eventClickTd" = ([event, $1]) => context.inquiryAll("edit", this[`users.${$1}.id`]);
+  "#eventClickTd" = ([, $1]) => context.inquiryAll("edit", this[`users.${$1}.id`]);
 }
 
 const dependencyRules = [
