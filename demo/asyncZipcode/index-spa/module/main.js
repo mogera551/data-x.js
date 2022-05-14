2e0b4eb5317cb3c2ab1a6c01c07e209f55b5c0e6
mogera551 <mogera551@gmail.com>
2022-05-14 19:26:32 +0900

    [add]demo, async get zipcode

---
+++ demo/asyncZipcode/index-spa/module/main.js
@@ -0,0 +1,26 @@
+const URL_API = "https://api.zipaddress.net/";
+
+class AppViewModel {
+  "@@inputZip"  = "";
+  "@zipcode#get" = () => !isNaN(this.inputZip) && this.inputZip.length === 7 ? this.inputZip : null;
+  "@address#get" = async() => {
+    if (this.zipcode == null) return "";
+    const params = new URLSearchParams({ zipcode: this.zipcode });
+    return fetch(`${URL_API}?${params}`)
+      .then(response => response.json())
+      .then(json => json.code === 200 ? json.data.fullAddress : json.message);
+  }
+  "@message#get" = async() => {
+    const text = await this.address;
+    if (!text) return;
+    return `検索結果は、「${text}」です。`;
+  }
+}
+
+const dependencyRules = [
+  [ "zipcode", ["inputZip"] ],
+  [ "address", ["zipcode"] ],
+  [ "message", ["address"] ],
+];
+
+export default { AppViewModel, dependencyRules }
