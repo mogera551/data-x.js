51dc7156266b74283593433ecb81546cdc592d6f
mogera551 <mogera551@gmail.com>
2022-05-16 08:23:59 +0900

    [add] demo chart

---
+++ demo/chart/index-spa/module/main.js
@@ -0,0 +1,45 @@
+const context = {};
+class AppViewModel {
+  "@@chart";
+  initChart() {
+    const canvas = context.rootElement.getElementById('chart').getContext('2d');
+    this.chart = new Chart( canvas, {
+      type: 'bar',
+      data: {
+        labels: this.labels,
+        datasets: [ { label: 'example', data: this.values, }, ],
+      },
+    } );
+  };
+
+  "@labels" = ["red", "green", "blue"];
+  "@values" = [33, 55, 11];
+
+  "@datas#get" = () => this.labels;
+  "@datas.*.label#get" = () => this.labels[context.$1];
+  "@datas.*.value#get" = () => this.values[context.$1];
+
+  "@@label" = "";
+  "#eventClickAddItem" = () => {
+    this.labels.push(this.label);
+    this.values.push(Math.floor(Math.random() * 100));
+    this.label = "";
+    this.chart.update();
+  }
+  "#eventClickDeleteItem" = ([, $1]) => {
+    this.labels.splice($1, 1);
+    this.values.splice($1, 1);
+    this.chart.update();
+  }
+  "#eventInit" = () => {
+    context.postUpdate(() => this.initChart());
+  }
+}
+
+const dependencyRules = [
+  [ "values", [ "eventClickAddItem", "eventClickDeleteItem" ] ],
+  [ "labels", [ "eventClickAddItem", "eventClickDeleteItem" ] ],
+  [ "datas", [ "labels" ] ],
+];
+
+export default { AppViewModel, context, dependencyRules }
